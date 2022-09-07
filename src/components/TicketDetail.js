0992e5aed6f57cbf7287400df96333fba5f13ad3
zbl14 <ifthereisoneday@gmail.com>
2022-09-07 14:13:50 -0700

    create a placeholder TicketDetail

---
+++ src/components/TicketDetail.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TicketDetail(props) {
+  return(
+    <React.Fragment>
+      <h1>Ticket Detail</h1>
+      <hr/>
+    </React.Fragment>
+  );
+};
+
+export default TicketDetail;
