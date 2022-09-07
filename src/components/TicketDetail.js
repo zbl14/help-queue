c14521ee49c88a78b357aa821cd59bef8f662722
zbl14 <ifthereisoneday@gmail.com>
2022-09-07 14:51:39 -0700

    update TicketDetail Componernt to Render SelectedTicket

--- src/components/TicketDetail.js
+++ src/components/TicketDetail.js
@@ -1,12 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const TicketDetail = (props) => {
+  const { ticket } = props;
   return (
     <React.Fragment>
       <h1>Ticket Detail</h1>
+      <h3>
+        {ticket.location} - {ticket.names}
+      </h3>
+      <p>
+        <em>{ticket.issue}</em>
+      </p>
       <hr />
     </React.Fragment>
   );
 };
 
+TicketDetail.propTypes = {
+  ticket: PropTypes.object,
+};
 export default TicketDetail;
