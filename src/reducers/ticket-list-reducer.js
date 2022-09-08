db9f53e88a1c9bc761532387ab8d501041a57b22
zbl14 <ifthereisoneday@gmail.com>
2022-09-08 14:25:27 -0700

    Should successfully delete a ticket

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -21,6 +21,10 @@ const reducer = (state = {}, action) => {
     //     id: id,
     //   },
     // });
+    case "DELETE_TICKET":
+      let newState = { ...state };
+      delete newState[id];
+      return newState;
     default:
       return state;
   }
