4784f57720a7d2d759a7ee58a844087ac1f7d3b6
zbl14 <ifthereisoneday@gmail.com>
2022-09-08 14:17:27 -0700

    Should successfully add new ticket data to mainTicketList

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,5 +1,29 @@
 const reducer = (state = {}, action) => {
-  return state;
+  const { names, location, issue, id } = action;
+  switch (action.type) {
+    case "ADD_TICKET":
+      return {
+        ...state,
+        ...{
+          [id]: {
+            names: names,
+            location: location,
+            issue: issue,
+            id: id,
+          },
+        },
+      };
+    // return Object.assign({}, state, {
+    //   [id]: {
+    //     names: names,
+    //     location: location,
+    //     issue: issue,
+    //     id: id,
+    //   },
+    // });
+    default:
+      return state;
+  }
 };
 
 export default reducer;
