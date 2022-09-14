ebc3b7ff451db848e072eeb811c81117047dc820
zbl14 <ifthereisoneday@gmail.com>
2022-09-13 20:09:56 -0700

    update action constants

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,7 +1,9 @@
+import * as c from "./../actions/ActionTypes";
+
 const reducer = (state = {}, action) => {
   const { names, location, issue, id } = action;
   switch (action.type) {
-    case "ADD_TICKET":
+    case c.ADD_TICKET:
       return {
         ...state,
         ...{
@@ -21,7 +23,7 @@ const reducer = (state = {}, action) => {
     //     id: id,
     //   },
     // });
-    case "DELETE_TICKET":
+    case c.DELETE_TICKET:
       let newState = { ...state };
       delete newState[id];
       return newState;
