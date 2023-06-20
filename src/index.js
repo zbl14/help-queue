1908fcb49634cf22376c19b4224fad1bb7570153
Zhibin Liang <ifthereisoneday@gmail.com>
2023-06-20 09:16:39 -0400

    replace createStore with configureStore

--- src/index.js
+++ src/index.js
@@ -2,18 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 // import reducer from "./reducers/ticket-list-reducer";
 import rootReducer from "./reducers/index";
 import { Provider } from "react-redux";
 
-const store = createStore(rootReducer);
+// const store = configureStore(rootReducer);
+const store = configureStore({ reducer: rootReducer });
 store.subscribe(() => console.log(store.getState()));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      {console.log(`store:`)}
+      {console.log(store)}
       <App />
     </Provider>
   </React.StrictMode>
